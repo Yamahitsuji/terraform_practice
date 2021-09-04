ed79168fcebfd7cb7da539884246f7ce9256310a
Yamahitsuji <goat06131004@gmail.com>
2021-09-04 23:53:50 +0900

    Finish chapter8

---
+++ chapter5/route53.tf
@@ -0,0 +1,35 @@
+data "aws_route53_zone" "example" {
+  name = "yamahitsuji.com"
+}
+
+resource "aws_route53_record" "example" {
+  zone_id = data.aws_route53_zone.example.zone_id
+  name    = data.aws_route53_zone.example.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.example.dns_name
+    zone_id                = aws_lb.example.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "example_certificate" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+
+  zone_id = data.aws_route53_zone.example.zone_id
+  ttl     = 60
+}
+
+output "domain_name" {
+  value = aws_route53_record.example.name
+}
