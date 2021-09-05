18104573ca21bb00ff3453d1e63af65a806c13fb
Yamahitsuji <goat06131004@gmail.com>
2021-09-05 22:28:05 +0900

    Finish chapter10

--- chapter5/ecs.tf
+++ chapter5/ecs.tf
@@ -70,3 +70,24 @@ module "ecs_task_execution_role" {
   identifier = "ecs-tasks.amazonaws.com"
   policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
+
+resource "aws_ecs_task_definition" "example_batch" {
+  container_definitions    = file("./batch_container_definitions.json")
+  family                   = "example-batch"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+}
+
+module "ecs_events_role" {
+  source     = "./iam_role"
+  name       = "ecs-events"
+  identifier = "events.amazonaws.com"
+  policy     = data.aws_iam_policy.ecs_events_role_policy.policy
+}
+
+data "aws_iam_policy" "ecs_events_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
+}
