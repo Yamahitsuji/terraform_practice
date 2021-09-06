78a261e2788d468f6676bd38255a894e70778d34
Yamahitsuji <goat06131004@gmail.com>
2021-09-06 23:12:05 +0900

    Finish chapter15

--- chapter5/s3.tf
+++ chapter5/s3.tf
@@ -80,3 +80,15 @@ resource "aws_s3_bucket" "artifact" {
     }
   }
 }
+
+resource "aws_s3_bucket" "operation" {
+  bucket = "operation-pragmatic-terraform"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
